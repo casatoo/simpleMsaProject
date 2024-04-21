5e8cb8bf301ff2fa9c452660ccb4d95a7e137b82
김민식B <casato6666@gmail.com>
2024-04-22 00:57:20 +0900

    게이트웨이 지역필터 설정
    구 gateway 모듈 삭제

---
+++ springCloudGateway/src/main/java/com/mskim/springcloudgateway/component/L1Filter.java
@@ -0,0 +1,51 @@
+package com.mskim.springcloudgateway.component;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.cloud.gateway.filter.GatewayFilter;
+import org.springframework.cloud.gateway.filter.factory.AbstractGatewayFilterFactory;
+import org.springframework.stereotype.Component;
+import reactor.core.publisher.Mono;
+
+@Component
+public class L1Filter extends AbstractGatewayFilterFactory<L1Filter.Config> {
+
+    public L1Filter() {
+
+        super(Config.class);
+    }
+
+
+    @Override
+    public GatewayFilter apply(Config config) {
+
+        return (exchange, chain) -> {
+
+            if (config.isPre()) {
+                // 지역필터 마이크로 서비스를 거치기 전
+                System.out.println("pre local filter 1");
+            }
+
+            return chain.filter(exchange)
+                    .then(Mono.fromRunnable(() -> {
+
+                        if (config.isPost()) {
+                            // 지역필터 마이크로 서비스를 거치고 난 후
+                            System.out.println("post local filter 1");
+                        }
+                    }));
+        };
+    }
+
+    @NoArgsConstructor
+    @AllArgsConstructor
+    @Data
+    public static class Config {
+        // pre 값이 트루인 경우
+        private boolean pre;
+        // post 값이 트루인 경우
+        private boolean post;
+    }
+
+}
