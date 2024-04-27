b6b1f1d6c219326ce9474d8f521bc5a5b6680da7
김민식B <casato6666@gmail.com>
2024-04-28 02:02:30 +0900

    소규모 사용자 사용 시 Inmemory 방식으로 사용자 등록하는 방식 주석처리

--- security/src/main/java/com/mskim/security/config/SecurityConfig.java
+++ security/src/main/java/com/mskim/security/config/SecurityConfig.java
@@ -4,7 +4,11 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -56,5 +60,23 @@ public class SecurityConfig {
 
         return http.build();
     }
+//      데이터베이스로 사용자 정보를 사용하지 않고 소수의 사용자를 등록하여 사용할 때
+//    @Bean
+//    public UserDetailsService userDetailsService() {
+//
+//        UserDetails user1 = User.builder()
+//                .username("user1")
+//                .password(bCryptPasswordEncoder().encode("1234"))
+//                .roles("ADMIN")
+//                .build();
+//
+//        UserDetails user2 = User.builder()
+//                .username("user2")
+//                .password(bCryptPasswordEncoder().encode("1234"))
+//                .roles("USER")
+//                .build();
+//
+//        return new InMemoryUserDetailsManager(user1,user2);
+//    }
 
 }
