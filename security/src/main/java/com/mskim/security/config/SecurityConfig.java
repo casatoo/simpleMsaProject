08207a84e44534809e78800d6c4574ebc22f7528
김민식B <casato6666@gmail.com>
2024-04-27 23:45:40 +0900

    세션 유지시간 30분 설정, 중복 세션, 고정 보호 적용

--- security/src/main/java/com/mskim/security/config/SecurityConfig.java
+++ security/src/main/java/com/mskim/security/config/SecurityConfig.java
@@ -29,13 +29,26 @@ public class SecurityConfig {
                 );
 
         http
-                .formLogin((auth) -> auth.loginPage("/security/login") // 자동으로 로그인 페이지로 이동
+                .formLogin((auth) -> auth
+                        .loginPage("/security/login") // 자동으로 로그인 페이지로 이동
                         .loginProcessingUrl("/security/loginProc") // 로그인 데이터를 넘겨 로그인처리를 진행
                         .permitAll()
                 );
 
         http
                 .csrf((auth) -> auth.disable()); // 위변조 방지를 위한 설정 임시 disable 처리
+
+        http
+                .sessionManagement((auth) -> auth
+                        .maximumSessions(1) // 하나의 아이디에 대한 다중로그인 허용 갯수
+                        .maxSessionsPreventsLogin(true)); // 다중로그인 갯수를 초과했을 때 처리 true : 새로운로그인 차단 , false : 기존 세션 하나 삭제
+
+        http
+                .sessionManagement((auth) -> auth
+                        .sessionFixation() // 세션 고정 공격을 방어하기 위한
+                        //.none()); // 로그인 시 세션 정보 변경 안함
+                        //.newSession()); // 로그인 시 세션 새로 생성
+                        .changeSessionId()); // 로그인 시 동일 세션에 대한 id 변경
 
         return http.build();
     }
