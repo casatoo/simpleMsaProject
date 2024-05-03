fa8417c93b3da920dadbd91fe5b0a7c9158865e7
김민식B <casato6666@gmail.com>
2024-05-04 01:32:44 +0900

    mongodb 저장 및 검색 테스트
    조회 메서드 추가 테스트

--- singleMongodb/src/main/java/com/mskim/singlemongodb/MainController.java
+++ singleMongodb/src/main/java/com/mskim/singlemongodb/MainController.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Controller
@@ -20,6 +21,33 @@ public class MainController {
     public List<User> MainP() {
 
         List<User> userList = userRepository.findAll();
+
+        return userList;
+    }
+
+    @GetMapping("/save/user")
+    @ResponseBody
+    public List<User> saveUser() {
+
+        User user = new User();
+        user.setData("2389");
+
+        userRepository.save(user);
+
+        List<User> userList = userRepository.findAll();
+
+        return userList;
+    }
+
+    @GetMapping("/find/user/data")
+    @ResponseBody
+    public List<User> findUser() {
+
+        List<User> userList = new ArrayList<>();
+
+        if (userRepository.existsByData("0000")) {
+            userList =  userRepository.findByData("2389");
+        }
 
         return userList;
     }
--- singleMongodb/src/main/java/com/mskim/singlemongodb/repository/UserRepository.java
+++ singleMongodb/src/main/java/com/mskim/singlemongodb/repository/UserRepository.java
@@ -3,5 +3,12 @@ package com.mskim.singlemongodb.repository;
 import com.mskim.singlemongodb.document.User;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
+import java.util.List;
+
 public interface UserRepository extends MongoRepository<User, String> {
+
+    List<User> findByData(String data);
+
+    Boolean existsByData(String data);
+
 }
